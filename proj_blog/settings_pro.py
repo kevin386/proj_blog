bf5a0150f0e02120d38fe6804e2f6d688ccdf41a
kevin <null386@gmail.com>
2014-08-15 22:20:22 +0800

    1. add product settings file.

---
+++ proj_blog/settings_pro.py
@@ -0,0 +1,41 @@
+"""
+Django settings for proj_blog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+from proj_blog.settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+import ConfigParser as CP
+configPath = os.path.join(BASE_DIR,'psw.cfg')
+config = CP.ConfigParser()
+with open(configPath, 'r') as cfgfile:
+    config.readfp(cfgfile)
+    user = config.get("dbinfo",'user')
+    psw = config.get("dbinfo",'psw')
+    host = config.get("dbinfo",'host')
+    port = config.get("dbinfo",'port')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'proj_blog',                      # Or path to database file if using sqlite3.
+        'USER': user,                      # Not used with sqlite3.
+        'PASSWORD': psw,                  # Not used with sqlite3.
+        'HOST': host,                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': port,                      # Set to empty string for default. Not used with sqlite3.
+    }
+}

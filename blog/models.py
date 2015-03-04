99cbe6540e2e17c78ab35f7b5b330b29657f1865
kevin <null386@gmail.com>
2015-03-05 00:10:32 +0800

    category的默认值

--- blog/models.py
+++ blog/models.py
@@ -73,6 +73,9 @@ class CategorySecond(models.Model):
         verbose_name="二级分类"
         verbose_name_plural="二级分类列表"
 
+def default_category():
+    return Category.objects.latest()
+
 class Category(models.Model):
     name = models.CharField("分类", max_length=64)
     sub_category = models.ForeignKey(CategorySecond, verbose_name='二级分类', blank=True, null=True)
@@ -93,7 +96,7 @@ class Tag(models.Model):
 class Article(models.Model):
     title = models.CharField("标题", max_length=128)
     #一个分类有多篇文章（多对一关系）
-    category = models.ForeignKey(Category,verbose_name="分类")
+    category = models.ForeignKey(Category,verbose_name="分类", default=default_category)
     #一篇文章可以有多个标签，一个标签也可以标记多篇文章（多对多关系）
     tags = models.ManyToManyField(Tag, verbose_name="标签", blank=True, null=True)
     content = models.TextField("内容")
